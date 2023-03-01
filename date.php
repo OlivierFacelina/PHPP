8cd5fea4e8c0918f23411cf11fdcfe3ea0a0f022
Olivier F <94624011+OlivierFacelina@users.noreply.github.com>
2023-03-01 11:44:04 +0100

    Correction exo date

--- date.php
+++ date.php
@@ -1,4 +1,5 @@
 <?php 
+date_default_timezone_set('Europe/Paris');
 $dt = time();
 $formatter = new IntlDateFormatter('fr_FR');
 $formatter -> setPattern('EEEE Q MMMM Y') ?>
@@ -25,44 +26,57 @@ $formatter -> setPattern('EEEE Q MMMM Y') ?>
                 <h2>Date du jour</h2>
 
                 <!-- 1. date du jour -->
-                <p class="text-muted">Nous sommes le : <?= date("d/m/y")?></p>
+                <p class="text-muted">Nous sommes le : 
+                <?= date("d/m/y")?></p>
 
                 <!-- 2. date du jour textuelle -->
-                <p class="text-muted">Nous sommes : <?= date("F j, Y, g:i a") ?></p>
+                <p class="text-muted">Nous sommes : 
+                <?= date("F j, Y, g:i a") ?></p>
 
                 <!-- 3. L'heure courante -->
                 <!-- Utilisation de la balise time (datetime prend date ISO ) -->
-                <p class="text-muted">Il est : <time datetime=""><?= date("H:i:s");?></time></p>
+                <p class="text-muted">Il est : <time datetime="">
+                <?= date("H:i:s");?></time></p>
 
                 <!-- 4. date en français -->
                 
                 <!-- Définir la configuration locale par défaut au fr  -->
-                <p class="text-muted">Nous sommes le : <?= $formatter->format($dt)?></p>
+                <p class="text-muted">Nous sommes le : 
+                <?= $formatter->format($dt)?></p>
                 <br>
                 
                 <!-- 5. Timestamp courant -->
                 <h2>Timestamp courant</h2>
-                <p class="text-muted">Timestamp courant : <?= time()?></p>
+                <p class="text-muted">Timestamp courant : 
+                <?= time()?></p>
                
                 <!-- 6. Calcul de date -->
                 <h2>Calcul de date</h2>
                 <!-- Faites vos calculs ici -->
-                <p class="text-muted">Nombre de jour dépuis le debut des cours : <?php 
-                $originalTime = new DateTimeImmutable("2022-09-27");
-                $targedTime = new DateTime();
-                $interval = $originalTime->diff($targedTime);
-                echo $interval->format("%a"), "\n"; 
+                <p class="text-muted">Nombre de jour dépuis le debut des cours : 
+                <?php 
+                    $originalTime = new DateTimeImmutable("2022-09-27");
+                    $targedTime = new DateTime();
+                    $interval = $originalTime->diff($targedTime);
+                    echo $interval->format("%a"), "\n"; 
+
+                    /* CORRECTION 
+                    $dateStartCourses = new DateTime('2022-09-27');
+                    $diff = date_diff($currentDate, $dateStartCourses);
+                    echo $diff->days */
                 ?></p>
                
                 <!-- 7. J + 25 -->
                
                <!-- Faites vos calculs ici -->
-                <p class="text-muted">La date J + 25 est : <?php 
-                $date = new DateTime();
-                $interval = date_interval_create_from_date_string('25 days');
-                $res = date_add($date, $interval);   
-                $format = date_format( $res, "d-m-Y H:i:s");
-                echo $format; ?></p>
+                <p class="text-muted">La date J + 25 est : 
+                <?php 
+                    $date = new DateTime();
+                    $interval = date_interval_create_from_date_string('25 days');
+                    $res = date_add($date, $interval);   
+                    $format = date_format( $res, "d-m-Y H:i:s");
+                    echo $format; 
+                ?></p>
                 
                 <!-- 8. J - 15 -->
                <!-- Faites vos calculs ici -->
@@ -76,21 +90,37 @@ $formatter -> setPattern('EEEE Q MMMM Y') ?>
                 
                 <!-- 9. Nb de jour dans le mois -->
                 <h2>Nombre de jour dans le mois</h2>
-                <p class="text-muted">Nombre de jour : <?= date("t") ?></p>
+                <p class="text-muted">Nombre de jour : 
+                <?= date("t")
+                // cal_days_in_month(CAL_GREGORIAN, $currentDate->format('m'))
+                ?>
+                </p>
                 
                 <!-- 10. Timestamp Epoch -->
                 <h2>Timestamp du 01/01/1970 00:00</h2>
-                <p class="text-muted">Nombre de jour : <?= mktime(0, 0, 0, 7, 1, 2000) ?></p>
+                <p class="text-muted">Timestamp : 
+                <?= mktime(0, 0, 0, 1, 1, 1970);
+                // echo strtotime('1970/01/01). ' - Paris';
+                // echo new DateTime('1970/01/01)->settimezone ' - Paris'
+                ?>
+                </p>
                 <hr>
                
                 <!-- 11 date du timestamp -->
                 <h2>Date du timestamp 1601532000</h2>
-                <p class="text-muted">Date correspondante est : <?php
+                <p class="text-muted">Date correspondante est : 
+                <?php
                 $timestamp = 1601532000;
                 // Le format
                 $format = "l d M Y à H:i:s";
                 // Formatage en jour, mois, année, heure, minutes et secondes
-                echo date($format, $timestamp); // -> Saturday 01 Jan 2022 à 12:30:1?></p>
+                echo date($format, $timestamp); // -> Saturday 01 Jan 2022 à 12:30:1
+
+                /* Correction 
+                echo date('d/m/Y - h:i:s', 1601532000)
+                */
+                ?>
+                </p>
                 
                 <!-- 12 calcul âge -->
                 <h2>Calcul de l'âge </h2>
