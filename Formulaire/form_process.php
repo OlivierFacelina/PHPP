c8bcb40e217bc43afb930fc687b014aa32b22b0b
Olivier F <94624011+OlivierFacelina@users.noreply.github.com>
2023-03-17 11:03:08 +0100

    First commit

---
+++ Formulaire/form_process.php
@@ -0,0 +1,5 @@
+<?php 
+
+$lastname = ''; $firstname = ''; $address = ''; $city = ''; $zipCode = ''; $phone = ''; $email = ''; $birthdate = ''; $birthPlace = '';
+$birthDepartment = ''; $birthCountry = ''; $nationality = ''; $message = ''; $formation = ''; $diploma = '';
+
