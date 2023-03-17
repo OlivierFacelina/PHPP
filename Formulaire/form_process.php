52f7630411ccf8b3de3c945cc7fa13a7ab3b3a3b
Olivier F <94624011+OlivierFacelina@users.noreply.github.com>
2023-03-17 11:38:36 +0100

    Input array

--- Formulaire/form_process.php
+++ Formulaire/form_process.php
@@ -3,3 +3,61 @@
 $lastname = ''; $firstname = ''; $address = ''; $city = ''; $zipCode = ''; $phone = ''; $email = ''; $birthdate = ''; $birthPlace = '';
 $birthDepartment = ''; $birthCountry = ''; $nationality = ''; $message = ''; $formation = ''; $diploma = '';
 
+$options = [
+    'lastname' => [
+        'filter' => FILTER_VALIDATE_REGEXP,
+        'options'=> [
+            'regexp' => '/^[a-z-èéùç ]{2,60}$/gi'
+        ]
+    ],
+    'firstname' => [
+        'filter' => FILTER_VALIDATE_REGEXP,
+        'options'=> [
+            'regexp' => '/^[a-z-èéùç ]{2,60}$/gi'
+        ]
+    ],
+    'address' => [
+        'filter' => FILTER_VALIDATE_REGEXP,
+        'options'=> [
+            'regexp' => '/^[a-z-èéùç0-9 ]{2,250}$/gi'
+        ]
+    ],
+    'city' => [
+        'filter' => FILTER_VALIDATE_REGEXP,
+        'options'=> [
+            'regexp' => '/^[a-z-èéùç ]{2,60}$/gi'
+        ]
+    ],
+    'zipCode' => [
+        'filter' => FILTER_VALIDATE_REGEXP,
+        'options'=> [
+            'regexp' => '/^[0-9]{5}$/gi'
+        ]
+    ],
+    'phone' => [
+        'filter' => FILTER_VALIDATE_REGEXP,
+        'options'=> [
+            'regexp' => '/[^0-9\-]/i'
+        ]
+    ],
+    'email' => FILTER_VALIDATE_EMAIL,
+    'birthdate' => [
+        'filter' => FILTER_VALIDATE_REGEXP,
+        'options'=> [
+            'regexp' => '/^[0-9]{4}(-[0-9]{2}){2}$/gmi'
+
+        ]
+    ],
+    'birthPlace' => FILTER_SANITIZE_SPECIAL_CHARS,
+    'birthDepartment' => FILTER_SANITIZE_SPECIAL_CHARS,
+    'birthCountry' => FILTER_SANITIZE_SPECIAL_CHARS,
+    'nationality' => FILTER_SANITIZE_SPECIAL_CHARS,
+    'message' => FILTER_SANITIZE_SPECIAL_CHARS,
+    'formation' => FILTER_SANITIZE_SPECIAL_CHARS,
+    'diploma' => FILTER_SANITIZE_SPECIAL_CHARS,
+];
+
+if(!empty($_POST)) {
+    $processedData = filter_input_array(INPUT_POST, $options);
+    var_dump($processedData);
+}
